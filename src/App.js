f756939022822ecad1d5b60d119f045452dfb757
Joydeep <joydeepmandal9999@gmail.com>
2021-10-01 09:50:20 +0530

    01/10/2021 - 09:50

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState } from 'react';
+
 
 import About from "./Components/About";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
 const App = () => {
+  const [mode, setMode] = useState('light');
+  setMode(setModes);
   return (
     <div>
       <Router>
-        <Navbar />
+        <Navbar modes={mode}/>
         <div className="container">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
         </div>
       </Router>
     </div>
